4b35a41abfeec38705aafc298a04a56349567d16
SergioRuval <ruvaloser@gmail.com>
2021-06-20 19:48:25 -0500

    Consultorio medico - front end - Consultorio medico con secciones para seleccionar el tipo de usuario y moverse entre las páginas de login, registro y la principal. Falta backend y páginas para los médicos y los enfermeros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPatientComponent } from './components/add-patient/add-patient.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'home', component: MainPageComponent },
+  { path: "addpatient", component: AddPatientComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddPatientComponent } from './components/add-patient/add-patient.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { VitalSignsComponent } from './components/vital-signs/vital-signs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainPageComponent,
+    AddPatientComponent,
+    LoginComponent,
+    SignupComponent,
+    VitalSignsComponent
   ],
   imports: [
     BrowserModule,
